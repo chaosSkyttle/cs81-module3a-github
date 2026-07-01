efa217c990fd03c5f91b45e1cca9ad2080c8d350
Skyler Stevenson <skylerstevenson@icloud.com>
2026-07-01 14:57:23 -0700

    "Added multiplyNum(), tellTime(), and quantity() functions to  journal"

--- journal.js
+++ journal.js
@@ -4,3 +4,25 @@ function greet(name){
     return "Hello, " + name + "!";
 }
 
+// Checks if a number is even or odd
+
+function isEven(num){
+    if(num%2 === 0){
+        return true;
+    }
+    else{
+        return false
+    }
+}
+
+// Multiplication function
+
+function multiplyNum(a = 1, b = 1){
+    return a * b;
+}
+
+// Tell the time
+
+function time(hour, minute, ampm){
+    return "It is ${hour}:${minute}${ampm}";
+}
